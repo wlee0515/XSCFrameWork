b19d6c7476f3d0c51dab1bfcb1085ce7873a4c67
wlee0515 <wlee0515@users.noreply.github.com>
2018-12-28 14:38:11 -0500

    Update buildScript.sh

--- Project/buildScript.sh
+++ Project/buildScript.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+BuildDir="build"
+
+if [ -d $BuildDir ]; then 
+  else
+    mkdir $BuildDir
+fi
+
 g++ -o build/XSCRun -I XSCFrameWork/include/ -I XSCFrameWork/thirdparty/ XSCFrameWork/source/*.cxx XSCFrameWork/thirdparty/*.cpp
