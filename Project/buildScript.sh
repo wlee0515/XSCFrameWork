22961b6e44d81fc64f8a6e71bce949ad30c65466
wlee0515 <wlee0515@users.noreply.github.com>
2018-12-28 14:44:19 -0500

    Update buildScript.sh

--- Project/buildScript.sh
+++ Project/buildScript.sh
@@ -3,8 +3,11 @@
 BuildDir="build"
 
 if [ -d $BuildDir ]; then 
-  else
-    mkdir $BuildDir
+   echo "Removing existing build directory {$BuildDir}"
+   sudo rm -r $LinkDir
 fi
+
+echo "Creatiing new build directory {$BuildDir}"
+sudo mkdir $BuildDir
 
 g++ -o build/XSCRun -I XSCFrameWork/include/ -I XSCFrameWork/thirdparty/ XSCFrameWork/source/*.cxx XSCFrameWork/thirdparty/*.cpp
